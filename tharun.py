b92d024d3dcdcac8e024e07bb255b519d227ae74
Tharunvallisetti <tharunmsda123@gmail.com>
2022-08-19 14:26:59 +0530

    adding changes files

--- tharun.py
+++ tharun.py
@@ -1,9 +1,13 @@
-def add(a, b):
-    if isinstance(a, int) and isinstance(b, int):
-        return a + b
-    else:
-        return ("it cannot perform")
-
-
-x = add(10, 20)
-print(x)
+def prime_number(N,i=2):
+    if N==1:
+        return False
+    if N==i:
+        return True
+    elif N%i==0:
+        return False
+    return prime_number(N,i+1)
+N=int(input("enter the number: "))
+if prime_number(N):
+    print(N,"is a prime number")
+else:
+    print(N,"is not a prime number")
